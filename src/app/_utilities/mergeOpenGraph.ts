c74a30290e2a51fb167e44f9152d17b7b4c744c4
Yunho <ycho0502@gmail.com>
2024-05-09 20:21:48 -0700

    metadata modification

--- src/app/_utilities/mergeOpenGraph.ts
+++ src/app/_utilities/mergeOpenGraph.ts
@@ -2,12 +2,12 @@ import type { Metadata } from 'next'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  siteName: 'Payload E-Commerce Template',
-  title: 'Payload E-Commerce Template',
-  description: 'An open-source e-commerce store built with Payload and Next.js.',
+  siteName: 'Cottonbell',
+  title: 'Cottonbell',
+  description: 'Designed to keep you comfortable',
   images: [
     {
-      url: 'https://payloadcms.com/images/og-image.jpg',
+      url: 'https://m.media-amazon.com/images/S/aplus-seller-content-images-us-east-1/ATVPDKIKX0DER/AF9P66JN7KVSA/40842df8-526c-4383-8139-1e934d65a25a._CR0,16,1829,549_PT0_SX600__.png',
     },
   ],
 }
