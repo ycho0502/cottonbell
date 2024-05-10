72544d6046ecacce6d3fd0ea88cac54cc07a3744
Yunho <39847161+ycho0502@users.noreply.github.com>
2024-05-10 12:38:55 -0700

    Update index.tsx
    Category -> Categories

--- src/app/_components/Categories/index.tsx
+++ src/app/_components/Categories/index.tsx
@@ -10,7 +10,7 @@ const Categories = ({ categories }: { categories: Category[] }) => {
   return (
     <section className={classes.container}>
       <div className={classes.titleWrapper}>
-        <h3>Shop by Categories</h3>
+        <h3>Shop by Category</h3>
         <Link href="/products">Show All</Link>
       </div>
 
