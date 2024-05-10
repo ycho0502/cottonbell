32feed6d4d4c35de3642251f7f439e440aecdc5e
Yunho <39847161+ycho0502@users.noreply.github.com>
2024-05-10 10:20:14 -0700

    Update payload.config.ts

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -81,7 +81,7 @@ export default buildConfig({
     url: process.env.DATABASE_URI,
   }),
   // database-adapter-config-end
-  serverURL: "https://cottonbell.com",
+  serverURL: 'https://cottonbell.com',
   collections: [Pages, Products, Orders, Media, Categories, Users],
   globals: [Settings, Header, Footer],
   typescript: {
